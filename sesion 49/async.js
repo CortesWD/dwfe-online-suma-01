1b709077cbef9e8defb6300643ebf610186c38da
CortesWD <cccp.designer@gmail.com>
2021-07-22 20:02:44 -0500

    added doc

--- sesion 49/async.js
+++ sesion 49/async.js
@@ -1,15 +1,29 @@
 const app = document.getElementById('root');
 
-function request(param) {
-  return fetch(`https://api.giphy.com/v1/gifs/search?api_key=ErT4SajFmJKTRMjhTi1xTIYDMEm4DJED&q=${param}&limit=5`);
+/**
+ * @param {String} term - string term to query
+ * @returns {Promise} fetch Promise
+ */
+function request(term) {
+  return fetch(`https://api.giphy.com/v1/gifs/search?api_key=ErT4SajFmJKTRMjhTi1xTIYDMEm4DJED&q=${term}&limit=5`);
 };
 
+/**
+ * @description Image wrapper node creator
+ * @param {Object} props - element attributes
+ * @param {String} props.src - element attributes
+ * @returns {Node}
+ */
 const Img = ({ src }) => {
   const element = document.createElement('img');
   element.src = src;
   return element;
 };
 
+/**
+ * @description Async function to resolve requests
+ * @param {String} param - query term
+ */
 async function getGifs(param) {
   const getCat = await request(param);
   if(!getCat.ok) throw new Error('cannot fulfilled');
@@ -17,16 +31,20 @@ async function getGifs(param) {
   try {
     const { data } = await getCat.json();
 
-    //Recorremos los datos para apender el elemento
+    /**
+     * Recorremos los datos para apender el elemento
+     */
     (data || []).forEach(gif => {
-      // optional chaining para evitar break por key no encontrado
+      /**
+       * Se usa optional chaining para evitar error en app por key no encontrado
+       * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+       */
       const src = gif.images?.fixed_height?.url || '';
   
       app.appendChild(
         Img({ src })
       );
     });
-  
   } catch (error) {
     console.error(error);
     app.innerHTML = "Ocurrio un error";
